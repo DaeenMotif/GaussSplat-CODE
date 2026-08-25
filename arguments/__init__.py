3e9a0900a8166e4437951333411436d4e9525dbb
DaeenMotif <daeen.kabir@motifdrive.com>
2026-08-25 09:40:27 +0900

    Update arguments/__init__.py

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -20,10 +20,10 @@ class GroupParams:
 class ParamGroup:
     def __init__(self, parser: ArgumentParser, name : str, fill_none = False):
         group = parser.add_argument_group(name) # create a custom argument group with a name
-        for key, value in vars(self).items(): # list the arguments at cmd
+        for key, value in vars(self).items(): # key:value pairs of arguments and their values
             shorthand = False
             if key.startswith("_"): # for ModelParams Class below, check parameter names: startwith("_") [e.g. _source_path]
-                shorthand = True # we set shorthand true as a FLag and (next line)
+                shorthand = True # we set shorthand true as a Flag and (next line)
                 key = key[1:] # remove the '_'
             t = type(value) # value of the argument, e.g. sh_degree value is 3
             value = value if not fill_none else None # just affirmation its not None
@@ -64,9 +64,9 @@ class ParamGroup:
             ('port', 6009),
         ])
         '''
-        for arg in vars(args).items(): # 
+        for arg in vars(args).items(): 
             if arg[0] in vars(self) or ("_" + arg[0]) in vars(self):
-                setattr(group, arg[0], arg[1]) # set named attr to specific value
+                setattr(group, arg[0], arg[1]) # set the named attribute (arg[0]) on a given object (group) to specific value (arg[1])
         return group
 
 class ModelParams(ParamGroup): 
@@ -78,7 +78,7 @@ class ModelParams(ParamGroup):
         self._depths = "" # path to depth; not used for supervision in original 3DGS
         self._resolution = -1 # this specifies the resolution of loaded images (1, 1/2, 1/4, etc.)
         self._white_background = False # default BG set to black; except NeRF-Synthetic
-        self.train_test_exp = False # 
+        self.train_test_exp = False 
         self.data_device = "cuda"
         self.eval = False
         super().__init__(parser, "Loading Parameters", sentinel)
@@ -93,8 +93,8 @@ class PipelineParams(ParamGroup):
         self.convert_SHs_python = False # set True for pass with pytorch (not cuda)
         self.compute_cov3D_python = False # set True for pass with pytorch (not cuda)
         self.debug = False # debugging mode
-        self.antialiasing = False # low-pass filter (adds 0.3 variance) to prevent 
-        # sub-pixel Gaussians from causing aliasing effects
+        self.antialiasing = False # low-pass filter (adds 0.3 variance) to prevent aliasing
+        # sub-pixel Gaussians from causing aliasing effects 
         super().__init__(parser, "Pipeline Parameters")
 
 # Refer: Section 5 of the paper for details on the optimization parameters
@@ -106,19 +106,17 @@ class OptimizationParams(ParamGroup):
         self.position_lr_final = 0.0000016 # lr for 3D gaussian means at end (low at end)
         self.position_lr_delay_mult = 0.01 # this is the decay multiplier
         # for decay function, look at self.xyz_scheduler_args in gaussian_model.py and utils/general_utils.py
-        
         self.position_lr_max_steps = 30_000 # heuristic setting across all experiments in paper, allow more steps to better optimize
         self.feature_lr = 0.0025 # learning rate for the SH parameters, separate rates for different degrees
         self.opacity_lr = 0.025 # heuristic setting of opacity
         self.scaling_lr = 0.005 # heuristic setting of scaling vector of covariance matrix
-        self.rotation_lr = 0.001 # heuristic setting of quaternion vector of covariance matrix -
+        self.rotation_lr = 0.001 # heuristic setting of quaternion vector of covariance matrix
         
         # Exposure compensation learning rate parameters (additional implementation)
-        # self.exposure_lr_init = 0.01
-        # self.exposure_lr_final = 0.001
-        # self.exposure_lr_delay_steps = 0
-        # self.exposure_lr_delay_mult = 0.0
-        # deals with exposure changes in the different input images
+        self.exposure_lr_init = 0.01
+        self.exposure_lr_final = 0.001
+        self.exposure_lr_delay_steps = 0
+        self.exposure_lr_delay_mult = 0.0
         
         
         self.percent_dense = 0.01 # this is scaling factor for the scene-scale; important for xyz scaling
@@ -129,9 +127,9 @@ class OptimizationParams(ParamGroup):
         self.densify_until_iter = 15_000 # densify until 15K iterations, then stop densifying
         self.densify_grad_threshold = 0.0002 # max_grad: 0.0002 from code and paper
         
-        # Depth Regularization
-        # self.depth_l1_weight_init = 1.0
-        # self.depth_l1_weight_final = 0.01
+        # Depth Params
+        self.depth_l1_weight_init = 1.0
+        self.depth_l1_weight_final = 0.01
         
         self.random_background = False # set to False 
         self.optimizer_type = "default" # ADAM
